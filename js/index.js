621556c9031518bef0dcde7d66082f5f9375a586
Gakselrud <g.akselrud7@gmail.com>
2020-08-21 18:23:20 +0300

    Check My Work

--- js/index.js
+++ js/index.js
@@ -2,25 +2,20 @@ console.log('Execute Order 66');
 
 async function getGitUsers() {
     try {
-    const response = await fetch('https://api.github.com/users');
-    return await response.json()
-} 
-catch(err) {
-    alert(err);
-  }
-
+        const response = await fetch('https://api.github.com/users');
+        return await response.json()
+    }
+    catch (err) {
+        alert(err);
+    }
 };
 
 getGitUsers().then(users => {
     //console.log(users)
-    users.forEach(user => {  
+    users.forEach(user => {
         console.log(user);
-        document.body.innerHTML += `<div><a href=${user.url}>
-        ${user.login}>
-        ${user.avatar_url}>
-        ${user.followers_url}>
-        ${user.following_url}></a>
-        </div>`;
+        
+       
     })
 })
 
