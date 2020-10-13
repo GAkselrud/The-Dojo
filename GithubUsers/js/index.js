b23d4908b63760ae0c4479da00245da4d0c1e9b8
ВЯООКLУИ <64901218+GAkselrud@users.noreply.github.com>
2020-10-13 12:00:24 +0300

    Add files via upload

--- GithubUsers/js/index.js
+++ GithubUsers/js/index.js
@@ -1,8 +1,8 @@
-console.log('Execute Order 66');
+//console.log('Execute Order 66');
 const USERS_API = 'https://api.github.com/users'
 
-async function getGitUsers(url, count=30) {
-   console.log(url + `per_page=${count}`)
+async function getGitUsers(url, count = 30) {
+    console.log(url + `per_page=${count}`)
     try {
         const response = await fetch(url + `?per_page=${count}`);
         return await response.json()
@@ -12,32 +12,30 @@ async function getGitUsers(url, count=30) {
     }
 };
 
+
+
 getGitUsers(USERS_API).then(users => {
-    //console.log(users)
     users.forEach(user => {
-    console.log(users)
-       
-    let followerCount = getGitUsers(user.followers_url, 1000000).then(followers => {
-        return followers.length     
+        console.log(users)
 
-    })
+        getGitUsers(user.followers_url, 1000000).then(followers => {
+            let countFollowers = followers.length >= 100 ? "100+" : followers.length
+            getGitUsers(user.followers_url, 1000000).then(following => {
+                let countFollowing = following.length >= 100 ? "100+" : following.length
+                document.querySelector('.container').innerHTML += `
 
-
-
-        document.body.innerHTML += `
-        <section>
-        <img src="${user.avatar_url}">
         
-        <h2>${user.login}</h2>
-        <p>${followerCount}</p>
-        <a href=${user.html_url}>Go to profile</a>
-        </section>`;
-       
-         
-       
+        <div class="card">
+        <img class="card__pic" src="${user.avatar_url}">
+        <h2 class="card__username">${user.login}</h2>
+        <p class="card__followers"><a href=${user.followers_url} target="_blank"> Followers</a> : ${countFollowers}</p>
+        <p class="card__following"><a href=${user.following_url} target="_blank"> Following</a> : ${countFollowing}</p>
+        <p class="card__link"><a href=${user.html_url} target="_blank">Go to Profile</a></p>
+        </div>`;
+            })
+        })
     })
-    })
-
+});
 
 
 
